b865c66ad08d92a317333a29d29ed65c7333d512
xKachi <kindtheideator@gmail.com>
2024-02-29 10:03:38 +0100

    Initialized Project

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+
+function Homepage() {
+  return (
+    <div className="test">
+      <PageNav />
+      <AppNav />
+      <h1>WorldWise</h1>
+
+      <Link to="/app">Go to the app</Link>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav /> NOT FOUND
+    </div>
+  );
+}
+
+export default PageNotFound;
